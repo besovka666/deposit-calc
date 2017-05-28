8c1771858f60e6b9033a3a65e57c15591c40878d
besovka666 <martapopa1998@gmail.com>
2017-05-29 00:03:53 +0700

    Combining 2 func in 1

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "deposit.h"
 
 int main() {
@@ -6,14 +7,13 @@ int main() {
 	scanf("%d", &length);
 	printf("enter dep_amount to deposit: \n");
 	scanf("%d", &dep_amount);
-	n = data(length);
-	if (n==0) 
-		printf("Error: Incorrect length. \n");
-	n=money(dep_amount);
-	if (n==0) 
-		printf("Error: Incorrect dep_amount \n"); 
-	printf("dep_amount at the end of: ");
-	printf("%d \n", amount_of_income(length, dep_amount));
-	printf("net profit: %d \n", amount_of_income(length, dep_amount)-dep_amount);
+	if (checkdata(length, dep_amount)==0)
+       		printf("Error, please enter correct data! \n");
+	else
+	{
+		printf("dep_amount at the end of: ");
+		printf("%d \n", amount_of_income(length, dep_amount));
+		printf("net profit: %d \n", amount_of_income(length, dep_amount)-dep_amount);
+	}
 return 0;
 }
